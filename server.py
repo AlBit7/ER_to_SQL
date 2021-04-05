6cedbfcddfcf5b18520d3f7eb1c41c236ffb3dd5
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2021-04-05 14:26:53 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,80 @@
-import socket, threading, requests
+import http.server, socketserver, os.path, cgi
 from main import *
-import os.path
+
+PORT = 8080
+SERVER = '192.168.1.108'
+FORMATO = 'utf-8'
+PATH = os.path.dirname(__file__).replace("\\", "/")
+
+class handler(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+        
+        if self.path == "/":
+            self.path = "/index.html"
+
+        try:
+
+            file = open(PATH + self.path, 'r')
+            html = file.read()
+            file.close()
+
+            self.send_response(200)
+
+        except:
+
+            html = """<p>Pagina non trovata, ritorna a&nbsp;</p><a href="/">casa</a>"""
+            
+            self.send_response(404)
+        
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(html.encode(FORMATO))
+
+    def do_POST(self):
+
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST', 'CONTENT_TYPE':self.headers['Content-Type']}
+        )
+
+        try:
+            
+            payload = form.getvalue("fileDaParsare").decode('utf-8-sig')
+            er = ER(payload)
+            html = "<p>" + er.displaySQL().replace("\n", "<br>").replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;") + "</p>"
+            self.send_response(200)
+        
+        except:
+
+            self.send_response(500)
+            html = "<p>errore server o file corrotto</p>"
+
+        
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(html.encode(FORMATO))
+
+def main():
+    
+    try:
+
+        with socketserver.TCPServer((SERVER, PORT), handler) as httpd:
+            print("server in ascolto su " + SERVER + ":" + str(PORT) + "...")
+            httpd.serve_forever()
+
+    except KeyboardInterrupt:
+        
+        print('spegnimento...')
+        httpd.server_close()
+
+if __name__ == '__main__':
+    main()
+
+"""
+import socket, threading, os.path
+from main import *
 
 PATH = os.path.dirname(__file__).replace("\\", "/")
 PORT = 8080
@@ -25,33 +99,43 @@ def serviCliente(connessione, richiesta, address):
     if paginaRichiesta == "/":
         paginaRichiesta = "/index.html"
 
+    # prova a leggere il file richiesto
     try:
         file = open(PATH + paginaRichiesta, 'r')
         html = file.read()
         file.close()
         header = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
-    
+
     except:
         header = 'HTTP/1.1 404 NOT FOUND\nContent-Type: text/html\n\n'
-        html = "<p>Pagina '" + paginaRichiesta + "' non trovata</p>"
-    
+        html = "<p>Pagina non trovata</p><br><a href="/">casa</a>"
+
+    if method == 'GET':
+        pass
+
     if method == 'POST':
 
-        try:
-            nomeFile = richiesta.split("\r\n")[-1][2:]
-            contenutoFile = open(nomeFile, 'r')
-            print("contenuto: "+contenutoFile.read())
-            er = ER(contenutoFile.read())
-            html += "<p>" + er.displaySQL() + "</p>"
-            contenutoFile.close()
+        header = 'HTTP/1.1 200\nContent-Type: text/html\n\n'
+        html += "ciao"
 
-        except:
-            header = 'HTTP/1.1 500\nContent-Type: text/html\n\n'
-            html += "<p>errore server</p>"
+        # try:
+        #     # nomeFile = richiesta.split("\r\n")[-1][2:]
+        #     # contenutoFile = 0# ??open(nomeFile, 'r').read()
+        #     # print("contenuto: "+contenutoFile)
+        #     # er = ER(contenutoFile)
+        #     # html += "<p>" + er.displaySQL() + "</p>"
+        #     # contenutoFile.close()
+        #     header = 'HTTP/1.1 200\nContent-Type: text/html\n\n'
+        #     html += "<p>ciao</p>"
+
+        # except:
+            
+        #     header = 'HTTP/1.1 200\nContent-Type: text/html\n\n'
+        #     html += "<p>errore server</p>"
 
     risposta = header + html
 
-    connessione.send(risposta.encode(FORMATO))
+    connessione.sendall(risposta.encode(FORMATO))
     connessione.close()
 
 while True:
@@ -59,10 +143,13 @@ while True:
     connessione, address = soc.accept()
     richiestaBrowser = connessione.recv(BUFFER).decode(FORMATO)
 
-    print(str(address[0]) + ":\n" + richiestaBrowser)
+    if richiestaBrowser != "":
 
-    thread = threading.Thread(target = serviCliente(connessione, richiestaBrowser, address))
-    thread.deamon = True
-    thread.start()
+        print(str(address[0]) + ":\n" + richiestaBrowser)
 
-    print('\nconnessioni attive ' + str(threading.activeCount()) + '\n')
+        thread = threading.Thread(target = serviCliente(connessione, richiestaBrowser, address))
+        thread.deamon = True
+        thread.start()
+
+        print('\nconnessioni attive ' + str(threading.activeCount()) + '\n')
+"""
